21e35a36b1818b707476d921c8b6c20098a8ec8d
Darya Bushik <bushik.darya@gmail.com>
2023-03-02 22:36:41 +0300

    added final version

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -2,9 +2,14 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HomePage extends BasePage {
+
     private final By loginButton = By.id("login");
+    private final By booksWrapper = By.xpath("//div[@class='rt-tbody']");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -14,5 +19,16 @@ public class HomePage extends BasePage {
         waitElementIsVisible(driver.findElement(loginButton));
         driver.findElement(loginButton).click();
         logger.info("Login page was opened.");
+    }
+
+    public boolean areBooksMatch(List<String> booksFromApi) {
+        List<WebElement> uiBooks = driver.findElements(booksWrapper);
+        for (String book : booksFromApi) {
+            if (uiBooks.contains(book)) {
+                return true;
+            }
+            logger.info("Books are matched.");
+        }
+        return false;
     }
 }
