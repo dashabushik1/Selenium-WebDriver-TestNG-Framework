1dc13c85e5753c53f75871333096067511deb7b8
Darya Bushik <bushik.darya@gmail.com>
2023-03-02 22:15:07 +0300

    added final version

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,33 +1,15 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LogOutPage;
 import pages.LoginPage;
 
-public class LoginTest {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://www.demoqa.com/books");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+public class LoginTest extends BaseTest {
 
     @Test
     public void isLoginSuccessful() throws InterruptedException {
-        HomePage home = new HomePage(driver);
-        LoginPage login = new LoginPage(driver);
-        LogOutPage dashboard = new LogOutPage(driver);
+        home = new HomePage(driver);
+        login = new LoginPage(driver);
+        logOut = new LogOutPage(driver);
 
         home.clickLoginButton();
 
@@ -37,27 +19,6 @@ public class LoginTest {
         login.clickLogin();
         Thread.sleep(3000);
 
-        System.out.println("The page heading is --- " + dashboard.getHeading());
-
-        dashboard.clickLogout();
-
-        driver.quit();
+        logOut.clickLogOut();
     }
-
-
-//    @Test
-//    public void loginSuccessful() {
-//        RestAssured.baseURI = "https://demoqa.com/swagger/";
-//        RequestSpecification request = RestAssured.given();
-//
-//        JSONObject requestParams = new JSONObject();
-//        requestParams.put("UserName", "Test-01");
-//        requestParams.put("Password", "Password!1");
-//
-//        request.body(requestParams.toString());
-//        Response response = request.post("/Account/v1/User");
-//
-//        Assert.assertEquals(response.getStatusCode(), 201);
-//        String userID = response.getBody().jsonPath().getString("userID");
-//    }
 }
